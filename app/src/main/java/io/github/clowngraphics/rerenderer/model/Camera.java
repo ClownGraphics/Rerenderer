af698e72910fc2dfd6c7a55832d30b2c9605048f
Lomtick1234 <74258123+Lomtick1234@users.noreply.github.com>
2025-01-06 19:42:11 +0300

    add Camera implement Camera vectors

--- app/src/main/java/io/github/clowngraphics/rerenderer/model/Camera.java
+++ app/src/main/java/io/github/clowngraphics/rerenderer/model/Camera.java
@@ -1,7 +1,46 @@
 package io.github.clowngraphics.rerenderer.model;
 
+import io.github.alphameo.linear_algebra.vec.Vec;
+import io.github.alphameo.linear_algebra.vec.Vec3;
+import io.github.alphameo.linear_algebra.vec.Vec3Math;
+import io.github.alphameo.linear_algebra.vec.Vector3;
+import io.github.clowngraphics.rerenderer.math.affine_transform.ScalarProjection;
+import io.github.clowngraphics.rerenderer.model.transform.CameraTransform;
+import io.github.clowngraphics.rerenderer.model.transform.ScreenTransform;
+
 public class Camera {
+    CameraTransform cameraTransform;
+    ScreenTransform screenTransform;
 
+    private Vector3 up = new Vec3(0,1, 0);
+    private Vector3 eye;
+    private Vector3 target = new Vec3(0,0, 1);
+    private Vector3 xAxis;
+    private Vector3 yAxis;
+    private Vector3 zAxis;
 
+    public Camera(Vector3 eye) {
+        this.eye = eye;
+    }
 
+    public Camera(Vector3 eye, Vector3 target) {
+        this.eye = eye;
+        this.target = target;
+    }
+
+    public Camera(Vector3 up, Vector3 eye, Vector3 target) {
+        this.up = up;
+        this.eye = eye;
+        this.target = target;
+    }
+
+    private void updateVectors(){
+        zAxis = Vec3Math.sub(target, eye);
+        xAxis = Vec3Math.cross(up, zAxis);
+        yAxis = Vec3Math.cross(zAxis, xAxis);
+        xAxis = Vec3Math.normalize(xAxis);
+        yAxis = Vec3Math.normalize(yAxis);
+        zAxis = Vec3Math.normalize(zAxis);
+
+    }
 }

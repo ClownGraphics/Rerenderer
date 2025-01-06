eef8eda05612a112e3344768440dfdb84ff555be
Lomtick1234 <74258123+Lomtick1234@users.noreply.github.com>
2025-01-06 19:21:23 +0300

    change Transformer

--- app/src/main/java/io/github/clowngraphics/rerenderer/model/transform/Transformer.java
+++ app/src/main/java/io/github/clowngraphics/rerenderer/model/transform/Transformer.java
@@ -1,4 +1,34 @@
 package io.github.clowngraphics.rerenderer.model.transform;
 
+import io.github.clowngraphics.rerenderer.model.Camera;
+import io.github.clowngraphics.rerenderer.model.Model;
+
 public class Transformer {
+    private Model model;
+    private Camera camera;
+
+    public Transformer(Model model, Camera camera) {
+        this.model = model;
+        this.camera = camera;
+    }
+    //todo: а преобразования применяются только к вершинам, или ещё к нормалям, или как...
+    public void transform(){
+
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public Camera getCamera() {
+        return camera;
+    }
+
+    public void setCamera(Camera camera) {
+        this.camera = camera;
+    }
 }

2d9c08bd3a41400854a72ad0d60e83774a3018ad
Lomtick1234 <74258123+Lomtick1234@users.noreply.github.com>
2025-01-07 20:09:58 +0300

    add MathTest

---
+++ app/src/test/java/io/github/clowngraphics/rerenderer/math/affine_transform/MathTest.java
@@ -0,0 +1,10 @@
+package io.github.clowngraphics.rerenderer.math.affine_transform;
+
+import org.junit.jupiter.*;
+import org.junit.jupiter.api.Test;
+
+public class MathTest {
+    @Test
+    void TransformTest() {
+    }
+}

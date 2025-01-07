a490bf16eeb36612294c3c28f7eb1853962b1346
Lomtick1234 <74258123+Lomtick1234@users.noreply.github.com>
2025-01-07 17:06:32 +0300

    change ScalarProjection

--- app/src/main/java/io/github/clowngraphics/rerenderer/math/affine_transform/ScalarProjection.java
+++ app/src/main/java/io/github/clowngraphics/rerenderer/math/affine_transform/ScalarProjection.java
@@ -7,8 +7,17 @@ import io.github.alphameo.linear_algebra.vec.Vector3;
 public class ScalarProjection implements Transformation {
 
     private Matrix4 transformationMatrix;
+    private Vector3 vx;
+    private Vector3 vy;
+    private Vector3 vz;
 
     public ScalarProjection(Vector3 vx, Vector3 vy, Vector3 vz) {
+        this.vx = vx;
+        this.vy = vy;
+        this.vz = vz;
+        recalculateMatrix();
+    }
+    private void recalculateMatrix(){
         transformationMatrix = new Mat4(new float[][]{
                 {vx.x(), vx.y(), vx.z(), 0},
                 {vy.x(), vy.y(), vy.z(), 0},
@@ -16,9 +25,32 @@ public class ScalarProjection implements Transformation {
                 {0, 0, 0, 1}
         });
     }
-
     @Override
     public Matrix4 getMatrix() {
         return transformationMatrix;
     }
+    public Vector3 getVx() {
+        return vx;
+    }
+
+    public void setVx(Vector3 vx) {
+        this.vx = vx;
+    }
+
+    public Vector3 getVy() {
+        return vy;
+    }
+
+    public void setVy(Vector3 vy) {
+        this.vy = vy;
+    }
+
+    public Vector3 getVz() {
+        return vz;
+    }
+
+    public void setVz(Vector3 vz) {
+        this.vz = vz;
+    }
+
 }

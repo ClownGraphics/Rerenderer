4cf1b88bdd76a98a55b4fda8f25e8c22cf34b320
Lomtick1234 <74258123+Lomtick1234@users.noreply.github.com>
2025-01-07 17:10:06 +0300

    add CombinedTransformation

--- app/src/main/java/io/github/clowngraphics/rerenderer/math/affine_transform/Projection.java
+++ app/src/main/java/io/github/clowngraphics/rerenderer/math/affine_transform/Projection.java
@@ -3,6 +3,13 @@ package io.github.clowngraphics.rerenderer.math.affine_transform;
 import io.github.alphameo.linear_algebra.mat.Matrix4;
 
 public class Projection implements Transformation{
+    private Matrix4 transformationMatrix;
+
+
+    public Projection() {
+        this.transformationMatrix = transformationMatrix;
+    }
+
     @Override
     public Matrix4 getMatrix() {
         return null;

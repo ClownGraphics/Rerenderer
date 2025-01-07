883f9d9ca7a3d2bd7908eb085e74adf905158500
Lomtick1234 <74258123+Lomtick1234@users.noreply.github.com>
2025-01-07 18:37:16 +0300

    change ScreenTransform delete Projection

--- app/src/main/java/io/github/clowngraphics/rerenderer/model/transform/ScreenTransform.java
+++ app/src/main/java/io/github/clowngraphics/rerenderer/model/transform/ScreenTransform.java
@@ -1,4 +1,39 @@
 package io.github.clowngraphics.rerenderer.model.transform;
 
-public class ScreenTransform {
+import io.github.alphameo.linear_algebra.mat.Mat4;
+import io.github.alphameo.linear_algebra.mat.Matrix4;
+import io.github.clowngraphics.rerenderer.math.affine_transform.Transformation;
+import io.github.clowngraphics.rerenderer.model.camera.CameraProperties;
+
+public class ScreenTransform implements Transformation {
+    private Matrix4 transformationMatrix;
+    private CameraProperties properties;
+
+    public ScreenTransform(CameraProperties cameraProperties) {
+        properties = cameraProperties;
+        recalculateMatrix();
+    }
+
+    public void setProperties(CameraProperties properties) {
+        this.properties = properties;
+        recalculateMatrix();
+    }
+    private void recalculateMatrix(){
+        float fov = properties.getFov();
+        float ar = properties.getAr();
+        float f = properties.getF();
+        float n = properties.getN();
+        float tanFov = (float) Math.tan(fov);
+        float invTanFov = 1 / tanFov;
+        transformationMatrix = new Mat4(new float[][]{
+                {invTanFov, 0, 0, 0},
+                {0, invTanFov / ar, 0, 0},
+                {0, 0, (f + n) / (f - n), (2 * f * n) / (n - f)},
+                {0, 0, 1, 0}
+        });
+    }
+    @Override
+    public Matrix4 getMatrix() {
+        return transformationMatrix;
+    }
 }

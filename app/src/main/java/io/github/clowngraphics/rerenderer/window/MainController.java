69a6f4a333923b0c790188271ae062eebe61c08b
vladSen <vlad.senyushkin@bk.ru>
2025-01-10 03:15:54 +0300

    change controller

--- app/src/main/java/io/github/clowngraphics/rerenderer/window/MainController.java
+++ app/src/main/java/io/github/clowngraphics/rerenderer/window/MainController.java
@@ -1,8 +1,14 @@
 package io.github.clowngraphics.rerenderer.window;
 
+import io.github.clowngraphics.rerenderer.model.Model;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class MainController {
 
@@ -18,13 +24,42 @@ public class MainController {
     @FXML
     private Canvas canvas;
 
+
+
+    @FXML
+    AnchorPane anchorPane;
+
+
+    private Model mesh = null;
+
+
+
+    private Timeline timeline;
+
     @FXML
     private void initialize() {
+        anchorPane.prefWidthProperty().addListener((ov, oldValue, newValue) -> canvas.setWidth(newValue.doubleValue()));
+        anchorPane.prefHeightProperty().addListener((ov, oldValue, newValue) -> canvas.setHeight(newValue.doubleValue()));
 
-        closeMenuItem.setOnAction(event -> handleClose());
-        deleteMenuItem.setOnAction(event -> handleDelete());
-        aboutMenuItem.setOnAction(event -> handleAbout());
+        timeline = new Timeline();
+        timeline.setCycleCount(Animation.INDEFINITE);
+
+        KeyFrame frame = new KeyFrame(Duration.millis(15), event -> {
+            double width = canvas.getWidth();
+            double height = canvas.getHeight();
+
+            canvas.getGraphicsContext2D().clearRect(0, 0, width, height);
+            // camera.setAspectRatio((float) (width / height));
+
+            if (mesh != null) {
+               // RenderEngine.render(canvas.getGraphicsContext2D(), camera, mesh, (int) width, (int) height);
+            }
+        });
+
+        timeline.getKeyFrames().add(frame);
+        timeline.play();
     }
+
 
     private void handleClose() {
 
